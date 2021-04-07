c53f87f1a4943720ea620f04318637a788cd5800
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-04-07 15:10:26 +0200

    add. mounted

--- js/script.js
+++ js/script.js
@@ -4,12 +4,23 @@ el:'#root',
 
 data: {
 
+  mail: "",
+
+},
+
+mounted: function(){
+
+  let myThis = this;
+  axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  .then(function(reply){
+
+    myThis.mail = reply.data.response
+
+  });
 
 
-
-
-  
 }
+
 
 
 
